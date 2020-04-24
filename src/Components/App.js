af86a88cb388f645575cc9dfaefe3a63e1058c4a
Todd Walraven <cod4247@yahoo.com>
2020-04-24 11:12:22 -0700

    update app to show current components:

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,11 +2,17 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import TapForm from './TapForm';
+import TapList from './TapList';
+import KegDetails from './KegDetails';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <TapForm />
+      <TapList />
+      <KegDetails />
     </React.Fragment>
   )
  }
