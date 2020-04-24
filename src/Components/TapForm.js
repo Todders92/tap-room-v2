3f937f1bc0e5516f61f331f940573e350442c2cc
Todd Walraven <cod4247@yahoo.com>
2020-04-24 11:13:12 -0700

    add initial content to files

---
+++ src/Components/TapForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TapForm() {
+  return (
+    <h1>Tap Form</h1>
+  )
+}
+
+export default TapForm;
---
+++ src/Components/TapList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TapList() {
+  return (
+    <h1>Tap list</h1>
+  )
+}
+
+export default TapList;
