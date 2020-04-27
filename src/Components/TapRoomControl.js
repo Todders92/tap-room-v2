b3c6fb2e82f769665f5c7bd0649b7669666d12bc
Todd Walraven <cod4247@yahoo.com>
2020-04-26 17:16:13 -0700

    add click function and update render method to navigate to form

--- src/Components/TapRoomControl.js
+++ src/Components/TapRoomControl.js
@@ -11,16 +11,42 @@ class TapRoomControl extends React.Component {
     };
   }
 
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+  }
+
   render(){
     let currentlyVisibleState = null;
+    let addBeerButton = null;
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <TapForm />
     } else {
       currentlyVisibleState = <TapList />
+      addBeerButton = <button onClick={this.handleClick}>Add Beer</button>
     }
     return (
       <React.Fragment>
         {currentlyVisibleState}
+      </React.Fragment>
+    );
+  }
+
+  render(){
+    let currentlyVisibleState = null;
+    let buttonText = null; 
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <TapForm />;
+      buttonText = "Return to Beer List"; 
+    } else {
+      currentlyVisibleState = <TapList />;
+      buttonText = "Add Beer";
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
