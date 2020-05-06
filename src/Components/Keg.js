64eb430241b5119fc30fd3fd3fb22a6470dad6b2
Todd Walraven <cod4247@yahoo.com>
2020-05-05 18:48:30 -0700

    update keg component

--- src/Components/Keg.js
+++ src/Components/Keg.js
@@ -4,15 +4,21 @@ import PropTypes from "prop-types";
 function Keg(props) {
   return (
     <React.Fragment>
-      <div onClick= {() => props.whenKegClicked(props.id)}>
+      <div>
         <h3 className="clickable" >
           {props.name} - {props.brand}
         </h3>
         <p>
           <em>{props.price}</em>
         </p>
+        <p>{props.count}</p>
+          <div className="buttons">
+            <button onClick={()=> props.whenSellClicked(props.id)} type="submit">Sell Pint</button>
+            <button onClick={()=> props.whenRefillClicked(props.id)} type="submit">Refill Keg</button> 
+            <button onClick={() => props.whenKegClicked(props.id)} type="submit">Details</button> 
+          </div>
+        </div>
         <hr />
-      </div>
     </React.Fragment>
   );
 }
@@ -24,7 +30,9 @@ Keg.propTypes = {
   alcoholContent: PropTypes.string,
   count: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
-  whenKegClicked: PropTypes.func
+  whenKegClicked: PropTypes.func,
+  whenSellClicked: PropTypes.func,
+  whenRefillClicked: PropTypes.func
 };
 
 export default Keg;
